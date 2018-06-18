45db89dc4e6fecf1568412cf1e35eaf347e875df
Bartek Łukawski <bwmn.peter@gmail.com>
2018-06-18 20:41:12 +0200

    Simplify export mechanism, drop unused variables

--- cmake/ASIBOT_HMIDescribe.cmake
+++ cmake/ASIBOT_HMIDescribe.cmake
@@ -2,7 +2,6 @@
 export(PACKAGE ASIBOT_HMI)
 
 # Retrieve global properties.
-get_property(_common_includes GLOBAL PROPERTY ASIBOT_HMI_INCLUDE_DIRS)
 get_property(_exported_targets GLOBAL PROPERTY ASIBOT_HMI_TARGETS)
 
 # CMake installation path.
@@ -12,37 +11,12 @@ else()
     set(_cmake_destination ${CMAKE_INSTALL_LIBDIR}/cmake/ASIBOT_HMI)
 endif()
 
-# Create and install config files.
-include(CMakePackageConfigHelpers)
-
-# Set exported variables (build tree).
-set(ASIBOT_HMI_INCLUDE_DIR "${_common_includes}")
-set(ASIBOT_HMI_MODULE_DIR ${CMAKE_SOURCE_DIR}/cmake)
-
 # <pkg>Config.cmake (build tree).
-configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/templates/ASIBOT_HMIConfig.cmake.in
-                              ${CMAKE_BINARY_DIR}/ASIBOT_HMIConfig.cmake
-                              INSTALL_DESTINATION ${CMAKE_BINARY_DIR}
-                              INSTALL_PREFIX ${CMAKE_BINARY_DIR}
-                              PATH_VARS ASIBOT_HMI_INCLUDE_DIR
-                                        ASIBOT_HMI_MODULE_DIR
-                              NO_CHECK_REQUIRED_COMPONENTS_MACRO)
-
-# Set exported variables (install tree).
-set(ASIBOT_HMI_INCLUDE_DIR ${CMAKE_INSTALL_INCLUDEDIR})
-set(ASIBOT_HMI_MODULE_DIR ${CMAKE_INSTALL_DATADIR}/ASIBOT_HMI/cmake)
-
-# <pkg>Config.cmake (install tree).
-configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/templates/ASIBOT_HMIConfig.cmake.in
-                              ${CMAKE_BINARY_DIR}/ASIBOT_HMIConfig.cmake.install
-                              INSTALL_DESTINATION ${_cmake_destination}
-                              PATH_VARS ASIBOT_HMI_INCLUDE_DIR
-                                        ASIBOT_HMI_MODULE_DIR
-                              NO_CHECK_REQUIRED_COMPONENTS_MACRO)
+file(WRITE ${CMAKE_BINARY_DIR}/ASIBOT_HMIConfig.cmake
+     "include(\${CMAKE_CURRENT_LIST_DIR}/ASIBOT_HMITargets.cmake)")
 
 # Install <pkg>Config.cmake.
-install(FILES ${CMAKE_BINARY_DIR}/ASIBOT_HMIConfig.cmake.install
-        RENAME ASIBOT_HMIConfig.cmake
+install(FILES ${CMAKE_BINARY_DIR}/ASIBOT_HMIConfig.cmake
         DESTINATION ${_cmake_destination})
 
 # Export library targets if enabled.
