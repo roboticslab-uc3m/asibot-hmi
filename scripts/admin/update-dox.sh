28c35b5d8439a462a9aaccce131f2d7ba1f052fd
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2019-04-24 15:27:41 +0200

    Perform CMake configure step

--- scripts/admin/update-dox.sh
+++ scripts/admin/update-dox.sh
@@ -5,10 +5,13 @@
 # crontab -e
 # 30 2 * * * /your/command
 
-path="$HOME/asibot-hmi"
-echo "Update asibot-hmi..."
-git -C "$path" pull
-echo "Doxy asibot-hmi..."
-path="$path/doc/build"
-mkdir -p "$path"
-make -C "$path" clean && make -C "$path" dox
+proj="asibot-hmi"
+src="$HOME/$proj"
+build="$src/doc/build"
+
+echo "Update $proj..."
+git -C "$src" pull
+
+echo "Doxy $proj..."
+mkdir -p "$build"
+cmake -H"$src/doc" -B"$build" && make -C "$build" clean && make -C "$build" dox
